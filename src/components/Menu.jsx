902e8f7e1ca6baac3b2832a306a2154dd1161734
Pardeep Singla <116626777+pardeep6443@users.noreply.github.com>
2024-07-07 18:10:50 +0530

    Update Menu.jsx

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import logoimg from "/src/img/Deeptube.png";
+import logoimg from '../img/Deeptube.png';
 import HomeIcon from "@mui/icons-material/Home";
 import ExploreOutlinedIcon from "@mui/icons-material/ExploreOutlined";
 import SubscriptionsOutlinedIcon from "@mui/icons-material/SubscriptionsOutlined";
